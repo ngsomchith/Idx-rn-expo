eb59af637517041a0d6a05a0c33d1c093663d549
Somchith NGUYEN <somchithnguyen@macbook-prongs.home>
2024-12-08 17:35:20 +0100

    images in render

--- constants/images.ts
+++ constants/images.ts
@@ -1,13 +1,309 @@
 
-import laCuisiniere from '../assets/images/la_cuisiniere_Delicatessen.png';
-// import laCuisiniereDelicatessen from '../assets/images/.png';
-import image1achete1offert from '../assets/images/image-1achete-1offert.png';
-import livreur from '../assets/images/image-livreur-delicatessen.png';
-import pizzasDuPort from '../assets/images/PIZZAS DU PORT.jpg';
+import laCuisiniere from '@/assets/images/la_cuisiniere_Delicatessen.png';
+// import laCuisiniereDelicatessen from '@/assets/images/.png';
+import image1achete1offert from '@/assets/images/image-1achete-1offert.png';
+import livreur from '@/assets/images/image-livreur-delicatessen.png';
+import pizzasDuPort from '@/assets/images/PIZZAS DU PORT.jpg';
 
-import livreursToulon from '../assets/images/livreurs-a-TOULON.webp';
+import livreursToulon from '@/assets/images/livreurs-a-TOULON.webp';
+
+
+
+// import profile from "@/assets/images/kemal.png";
+// // import img1 from "@/assets/images/seller2/bi_cuon.png"
+
+import aloe_lychee from "@/assets/images/seller2/aloe_lychee.png";
+import aloe_lychee2 from "@/assets/images/seller2/aloe_lychee.png";
+import banh_beo from "@/assets/images/seller2/banh_beo.png";
+import banh_beo2 from "@/assets/images/seller2/banh_beo.png";
+import banh_canh from "@/assets/images/seller2/banh_canh.png";
+import bun_bo_hue from "@/assets/images/seller2/bun_bo_hue.png";
+import bun_bo_hue2 from "@/assets/images/seller2/bun_bo_hue2.png";
+import banh_cuon_barquette from "@/assets/images/seller2/banh_cuon.png";
+import banh_cuon2 from "@/assets/images/seller2/banh_cuon.png";
+import banh_xeo from "@/assets/images/seller2/banh_xeo.png";
+import banh_xeo2 from "@/assets/images/seller2/banh_xeo.png";
+import bi_cuon from "@/assets/images/seller2/bi_cuon.png";
+import blanc_saumon_avocat from "@/assets/images/seller2/blanc_saumon_avocat.png";
+import blanc_saumon_cheese from "@/assets/images/seller2/blanc_saumon_cheese.png";
+import blanc_spring from "@/assets/images/seller2/blanc_spring.png";
+import bo_bun_boeuf from "@/assets/images/seller2/bo_bun_boeuf.png";
+import promo_bo_bun_boeuf from "@/assets/images/seller2/promo_bo_bun_boeuf.png";
+import bo_bun_boeuf_nem from "@/assets/images/seller2/bo_bun_nem.png";
+import bun_cha_gio from "@/assets/images/seller2/bun_cha_gio.png";
+import bun_reu from "@/assets/images/seller2/bun_reu.png";
+import bun_reu2 from "@/assets/images/seller2/bun_reu.png";
+
+
+
+import nem_porc from "@/assets/images/seller2/nem_porc.png";
+import nem_tha_dua from "@/assets/images/seller2/nem_tha_dua.png";
+import nem_tha_dua2 from "@/assets/images/seller2/nem_tha_dua2.png";
+import cali_blanc from "@/assets/images/seller2/cali_blanc.png";
+
+import citrouille_au_flanc2 from "@/assets/images/seller2/citrouille_au_flanc.png";
+import citrouille_au_flanc from "@/assets/images/seller2/citrouille_au_flanc.png";
+import porc_laque from "@/assets/images/seller2/porc_laque.png";
+import cali_spring from "@/assets/images/seller2/cali_spring.png";
+import yakitory from "@/assets/images/seller2/yakitory.png";
+import panga_caramel from "@/assets/images/seller2/panga_caramel.png";
+// "nouilles_croustillantes_aux_crevettes"
+import nouilles_croustillantes_aux_crevettes2 from "@/assets/images/seller2/nouilles_croustillantes_aux_crevettes2.png";
+import nouilles_croustillantes_aux_crevettes from "@/assets/images/seller2/nouilles_croustillantes_aux_crevettes.png";
+import galettes_poissons_poulets_frits from "@/assets/images/seller2/galettes_poissons_poulets_frits.png";
+import lap_thon from "@/assets/images/seller2/lap_thon.png";
+import tempura_crevettes from "@/assets/images/seller2/tempura_crevettes.png";
+import logo from "@/assets/images/seller2/logo.png";
+import logo2 from "@/assets/images/seller2/logo2.png";
+import ha_khao from "@/assets/images/seller2/ha_khao.png";
+// import pho2 from "@/assets/images/seller2/coca.png";
+
+import soupe_pho from "@/assets/images/seller2/pho.png";
+import soupe_pho2 from "@/assets/images/seller2/pho.png";
+import coca from "@/assets/images/seller2/coca.png";
+import coca2 from "@/assets/images/seller2/coca.png";
+import maki_saumon from "@/assets/images/seller2/maki_saumon.png";
+import maki_saumon2 from "@/assets/images/seller2/maki_saumon.png";
+import oasis from "@/assets/images/seller2/oasis.png";
+import oasis2 from "@/assets/images/seller2/oasis.png";
+import evian from "@/assets/images/seller2/evian.png";
+import evian2 from "@/assets/images/seller2/evian.png";
+import fanta from "@/assets/images/seller2/fanta.png";
+import fanta2 from "@/assets/images/seller2/fanta.png";
+import perrier from "@/assets/images/seller2/perrier.png";
+import perrier2 from "@/assets/images/seller2/perrier.png";
+import eau_de_source from "@/assets/images/seller2/eau_de_source.png";
+import eau_de_source2 from "@/assets/images/seller2/eau_de_source2.png";
+// import boeuf_fromage from "@/assets/images/seller2/boeuf_fromage.png";
+import saumon_au_caramel from "@/assets/images/seller2/saumon_au_caramel.png";
+import poulets_panes_jap from "@/assets/images/seller2/poulets_panes_jap.png";
+import gyozas_poulet from "@/assets/images/seller2/gyozas_poulet.png";
+import porc_au_caramel from "@/assets/images/seller2/porc_au_caramel.png";
+import soupe_nouille_canard_parfumee from "@/assets/images/seller2/soupe_nouille_canard_parfumee.png";
+import canh_chua from "@/assets/images/seller2/canh_chua.png";
+import vitrineWeb from "@/assets/images/seller2/vitrineWeb.webp";
+import vitrineModeEmploi from "@/assets/images/seller2/vitrineModeEmploi.png";
+import riz_bo_kho from "@/assets/images/seller2/riz_bo_kho.png";
+import banh_canh2 from "@/assets/images/seller2/banh_canh.png";
+import poke_thon_saumon from "@/assets/images/seller2/poke_thon_saumon.png";
+import keang_phet_kay from "@/assets/images/seller2/keang_phet_kay.png";
+
+import sam_bo_luong from "@/assets/images/seller2/sam_bo_luong.png";
+import sam_bo_luong2 from "@/assets/images/seller2/sam_bo_luong.png";
+
+import cali_oignon_cheese_avocat_saumon from "@/assets/images/seller2/cali_oignon_cheese_avocat_saumon.png";
+import che_dau_trang from "@/assets/images/seller2/che_dau_trang.png";
+import che_thai from "@/assets/images/seller2/che_thai.png";
+import che_thai2 from "@/assets/images/seller2/che_thai.png";
+import cali_saumon_avocat from "@/assets/images/seller2/cali_saumon_avocat.png";
+import cali_saumon_avocat2 from "@/assets/images/seller2/cali_saumon_avocat.png";
+import xum_carotte from "@/assets/images/seller2/xum_carotte.png";
+import riz_aux_3_tresors from "@/assets/images/seller2/riz_aux_3_tresors.png";
+import riz_aux_3_tresors_poulet from "@/assets/images/seller2/riz_aux_3_tresors_poulet.png";
+import riz_aux_3_tresors2 from "@/assets/images/seller2/riz_aux_3_tresors.png";
+// import riz_aux_2_tresors2 from "@/assets/images/seller2/riz_aux_2_tresors2.png"
+import riz_aux_2_tresors from "@/assets/images/seller2/riz_aux_2_tresors.png";
+import echine_de_porc_au_caramel_et_poivre from "@/assets/images/seller2/echine_de_porc_au_caramel_et_poivre.png";
+// import cali_spring from "@/assets/images/seller2/cali_spring.png"
+import Cali_saumon_avocat_cheese from "@/assets/images/seller2/Cali_saumon_avocat_cheese.png";
+
+import pub_Delicatessen from "@/assets/images/seller2/pub_Delicatessen.png";
+
+import cuisiniere from "@/assets/images/seller2/la_cuisiniere_Delicatessen.png";
+import maki_thon_spicy from "@/assets/images/seller2/maki_thon_spicy.png"; 
+import cali_cheese_avocat_saumon from "@/assets/images/seller2/cali_cheese_avocat_saumon.png";
+import california_thon_avocat from "@/assets/images/seller2/california_thon_avocat.png";
+import maki_avocat_cheese from "@/assets/images/seller2/maki_avocat_cheese.png";
+import sashimi_saumon from "@/assets/images/seller2/sashimi_saumon.png";
+import sashimi_thon_saumon from "@/assets/images/seller2/sashimi_thon_saumon.png";
+import maki_thon from "@/assets/images/seller2/maki_thon0.png";
+import spring_saumon_avocat from "@/assets/images/seller2/springroll_saumon_avocat.png";
+import salade_de_saumon from "@/assets/images/seller2/salade_de_saumon.png";
+import vitrinePDJ from "@/assets/images/seller2/vitrinePDJ.png";
+import promoOuvert from "@/assets/images/seller2/promoOuvert.png";
+import soupe_raviolis_crevettes from "@/assets/images/seller2/soupe_raviolis_crevettes.png"
+import promo_bun_nem from "@/assets/images/seller2/promo_bun_nem.png"
+import promo_tartare from "@/assets/images/seller2/promo_tartare.png" 
+import springroll_veget from "@/assets/images/seller2/springroll_veget.png";
+
+
+import riz_sautes_crevettes from "@/assets/images/seller2/riz_sautes_crevettes.png"
+import sashimi_thon from "@/assets/images/seller2/sashimi_thon.png"
+import galette_poissons_poulets_portion from "@/assets/images/seller2/galette_poissons_poulets_portion.png"
+import boeuf_sate from "@/assets/images/seller2/boeuf_sate.png"
+import riz from "@/assets/images/seller2/riz.png"
+import riz2 from "@/assets/images/seller2/riz.png"
+
+import buy_and_gift from "@/assets/images/seller2/buy_and_gift.png"
+import buy_and_gift2 from "@/assets/images/seller2/buy_and_gift2.png"
+import saute_chouchou_oeuf from "@/assets/images/seller2/saute_chouchou_oeuf.png"
+// import  from "@/assets/images/seller2/.png"
+// import  from "@/assets/images/seller2/.png"
+// import  from "@/assets/images/seller2/.png"
+// import  from "@/assets/images/seller2/.png"
+// import  from "@/assets/images/seller2/.png"
+// import  from "@/assets/images/seller2/.png"
+
+
+
+import sushi_saumon from "@/assets/images/seller2/sushi_saumon.png"
+import riz_saute_saumon from "@/assets/images/seller2/riz_saute_saumon.png"
+// import promo_riz_saute_saumon from "@/assets/images/seller2/promo_riz_saute_saumon.png"
+import riz_crevettes_ail from "@/assets/images/seller2/riz_crevettes_ail.png"
+import promo_vermicelles_aux_saumons from "@/assets/images/seller2/promo_vermicelles_aux_saumons.png"
+
+import vermicelles_aux_saumons from "@/assets/images/seller2/vermicelles_aux_saumons.png"
+import maki_saumon_cheese from "@/assets/images/seller2/maki_saumon_cheese.png"
+import cali_oignon_cheese_avocat from "@/assets/images/seller2/cali_oignon_cheese_avocat.png"
+import blanc_saumon_cheese_avocat from "@/assets/images/seller2/blanc_saumon_cheese.png"
+import cali_thon_avocat    from "@/assets/images/seller2/cali_thon_avocat.png"
+import phat_thai_crevettes from "@/assets/images/seller2/phat_thai_crevettes.png"
+import rouleaux_de_printemps from "@/assets/images/seller2/rouleaux_de_printemps.png"
+import rouleaux_de_rintemps2 from "@/assets/images/seller2/rouleaux_de_printemps2.png"
+import un_plat_gratuit from "@/assets/images/seller2/un_plat_gratuit.png"
+import tartare_saumon_avocat from "@/assets/images/seller2/tartare_saumon_avocat.png"
+import Rouget_aux_gingembres from "@/assets/images/seller2/Rouget_aux_gingembres.png"
+import cuisine from "@/assets/images/seller2/cuisine.png"
+import cuisiniere_logo from "@/assets/images/seller2/cuisiniere_logo.png"
+import devie_logo from "@/assets/images/seller2/devie_logo.png"
+import springroll_saumon_cheese from "@/assets/images/seller2/springroll_saumon_cheese.png"
+import maki_avocat from "@/assets/images/seller2/maki_avocat.png"
+import maki_saumon_avocat  from "@/assets/images/seller2/maki_saumon_avocat.png"
+import saumon_terriyaki from "@/assets/images/seller2/saumon_terriyaki.png"
+import cali_oignon_cheese_saumon from "@/assets/images/seller2/cali_oignon_cheese_saumon.png"
+import carte_bleue from "@/assets/images/seller2/carte_bleue.png"
+import nem_poulet from "@/assets/images/seller2/nem_poulet.png"
+import poulet_caramel from "@/assets/images/seller2/poulet_caramel.png"
+import poulet_caramel_gingembre from "@/assets/images/seller2/poulet_caramel_gingembre.png"
+import poulet_biere from "@/assets/images/seller2/poulet_biere.png"
+import sambos from "@/assets/images/seller2/sambos.png"
+import Lychees from "@/assets/images/seller2/Lychees.png";
+import Lait_soja from "@/assets/images/seller2/Lait_soja.png"
+import Perles_coco from "@/assets/images/seller2/Perles_coco.png"
+import Nougats from "@/assets/images/seller2/Nougats.png"
+import sushi_saumonx6 from "@/assets/images/seller2/sushi_saumonx6.png"
+import banh_khot from "@/assets/images/seller2/banh_khot.png"
+import salmon_roll_cheese from "@/assets/images/seller2/salmon_roll_cheese.png"
+import f50moins10 from "@/assets/images/seller2/f50moins10.png"
+import menu_maki from "@/assets/images/seller2/menu_maki.png"
+
+import fonduduo from "@/assets/images/seller2/fonduduo.png"
+import menu_california from "@/assets/images/seller2/menu_california.png"
+import menu_spring from "@/assets/images/seller2/menu_spring.png"
+import menu_cali_blanc from "@/assets/images/seller2/menu_cali_blanc.png"
+import menu_salmon_blanc_cheese from "@/assets/images/seller2/menu_salmon_blanc_cheese.png"
+import menu_blanc_saumon from "@/assets/images/seller2/menu_blanc_saumon.png"
+import menu_cali_spring from "@/assets/images/seller2/menu_cali_spring.png"
+import menu_salmon_roll_cheese from "@/assets/images/seller2/menu_salmon_roll_cheese.png"
+import cali_chicken from "@/assets/images/seller2/cali_chicken.png"
+import plan_delicatessen from "@/assets/images/seller2/plan_delicatessen.png"
+// import  from "@/assets/images/seller2/.png"
+// import  from "@/assets/images/seller2/.png"
+
 
 export default {
+
     laCuisiniere,livreursToulon,
-    image1achete1offert, livreur, pizzasDuPort
-}
+    image1achete1offert, livreur, pizzasDuPort,
+  buy_and_gift,buy_and_gift2,saute_chouchou_oeuf,
+  promoOuvert,sushi_saumon,blanc_saumon_cheese_avocat,springroll_saumon_cheese,maki_avocat,
+  vitrinePDJ,riz_crevettes_ail,blanc_saumon_cheese,cali_thon_avocat,maki_saumon_avocat,riz, riz2,
+  vermicelles_aux_saumons,un_plat_gratuit,riz_sautes_crevettes,menu_blanc_saumon,
+  promo_vermicelles_aux_saumons,Rouget_aux_gingembres,springroll_veget,menu_salmon_blanc_cheese,
+  phat_thai_crevettes,rouleaux_de_printemps,rouleaux_de_rintemps2,
+  bun_bo_hue,promo_tartare,poulet_caramel_gingembre,menu_spring,
+  cali_blanc,riz_saute_saumon,saumon_terriyaki,Lait_soja,menu_california,
+  cali_spring,poulet_caramel,sambos,boeuf_sate,Lychees,sushi_saumonx6,menu_salmon_roll_cheese,
+  // promo_riz_saute_saumon,
+  cali_oignon_cheese_saumon,Perles_coco,Nougats,menu_cali_blanc,
+  blanc_spring,maki_saumon_cheese,sashimi_thon,menu_cali_spring,
+  tempura_crevettes,cali_oignon_cheese_avocat,
+  cali_cheese_avocat_saumon,galette_poissons_poulets_portion,
+  sashimi_saumon,promo_bun_nem,menu_maki,plan_delicatessen,
+  bun_reu,tartare_saumon_avocat,banh_khot,
+  bo_bun_boeuf,ha_khao,cuisine,f50moins10,
+  bo_bun_boeuf_nem,carte_bleue,cali_chicken,
+  bun_cha_gio,riz_aux_2_tresors,
+  sashimi_thon_saumon,nem_poulet,
+  bun_reu2,cuisiniere_logo,poulet_biere,
+  blanc_saumon_avocat,fonduduo,
+  california_thon_avocat,
+  nem_tha_dua,devie_logo,
+  maki_thon_spicy,
+  maki_thon,pub_Delicatessen,
+  nem_tha_dua2,salmon_roll_cheese,
+  yakitory,
+  bun_bo_hue2,promo_bo_bun_boeuf,
+  logo,
+  spring_saumon_avocat,
+  logo2,
+  nem_porc,
+  poulets_panes_jap,
+  poke_thon_saumon,
+  panga_caramel,
+  banh_cuon_barquette,
+  banh_cuon2,
+  lap_thon,
+  salade_de_saumon,
+  gyozas_poulet,
+  banh_xeo,
+  banh_xeo2,
+  cali_oignon_cheese_avocat_saumon,
+  banh_beo,
+  maki_avocat_cheese,
+  banh_beo2,
+  canh_chua,
+  nouilles_croustillantes_aux_crevettes,
+  nouilles_croustillantes_aux_crevettes2,
+  soupe_nouille_canard_parfumee,
+  soupe_pho,
+  maki_saumon,
+  maki_saumon2,
+  soupe_pho2,
+  cali_saumon_avocat,
+  cali_saumon_avocat2,
+  riz_bo_kho,
+  keang_phet_kay,
+  Cali_saumon_avocat_cheese,
+  saumon_au_caramel,
+  porc_au_caramel,
+  bi_cuon,
+  oasis,
+  oasis2,
+  fanta,
+  fanta2,
+  perrier,
+  perrier2,
+  coca,
+  eau_de_source,
+  eau_de_source2,
+  evian,soupe_raviolis_crevettes,
+  evian2,
+  coca2,
+  aloe_lychee,
+  aloe_lychee2,
+  porc_laque,
+  che_dau_trang,
+  che_thai,
+  che_thai2,
+  citrouille_au_flanc2,
+  citrouille_au_flanc,
+  echine_de_porc_au_caramel_et_poivre,
+  galettes_poissons_poulets_frits,
+  // galettes_poissons_poulets_frits2
+  banh_canh,
+  banh_canh2,
+  sam_bo_luong,
+  sam_bo_luong2,
+  riz_aux_3_tresors,
+  riz_aux_3_tresors2,
+  // riz_aux_2_tresors,
+  riz_aux_3_tresors_poulet,
+  // boeuf_fromage,
+  xum_carotte,
+  vitrineWeb,
+  vitrineModeEmploi,
+  cuisiniere,
+};
+
+
